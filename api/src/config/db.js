4cfd20a0def3f239bff80a5b2df463bc066b3f63
shoaibmehmood243 <shoaibmehmood065@gmail.com>
2025-07-01 13:58:49 +0500

    Added db confi

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -6,9 +6,19 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  port: process.env.DB_PORT,
+});
+
+pool.getConnection((err, conn) => {
+  console.log('Connecting to database....');
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  if(conn){
+    console.log("Database Connection established! ")
+    conn.release();
+  }  
 });
 
 module.exports = pool.promise();
